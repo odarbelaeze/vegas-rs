10351a2df827a2fdc3f912a7b1d49eb8b497856d
Oscar David Arbeláez <odarbelaeze@gmail.com>
2016-11-12 13:12:02 -0500

    Implement Heisenberg spin

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
 use std::ops::{Mul};
 
 
-trait Spin:
-// I want to write this condition here so that implementing mul for references
-// is defined and outputs f64
-    // Mul<&'a Self, Output = f64> + Sized
+trait Spin where
+    for<'a, 'b> &'a Self: Mul<&'b Self, Output = f64>
 {
 }
 
@@ -33,9 +31,27 @@ impl<'a, 'b> Mul<&'a IsingSpin> for &'b IsingSpin {
 }
 
 
+struct HeisenbergSpin([f64; 3]);
+
+
+impl Spin for HeisenbergSpin {}
+
+
+impl<'a, 'b> Mul<&'a HeisenbergSpin> for &'b HeisenbergSpin {
+    type Output = f64;
+
+    fn mul(self, other: &'a HeisenbergSpin) -> f64 {
+        let &HeisenbergSpin(_self) = self;
+        let &HeisenbergSpin(_other) = other;
+        _self.iter().zip(_other.iter()).map(|(a, b)| a * b).fold(0f64, |sum, i| sum + i)
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::IsingSpin;
+    use super::HeisenbergSpin;
 
     #[test]
     fn ising_spin_multiplies_correctly() {
@@ -43,5 +59,10 @@ mod tests {
         assert_eq!(&IsingSpin::Down  * &IsingSpin::Up, -1.0);
         assert_eq!(&IsingSpin::Up * &IsingSpin::Down, -1.0);
         assert_eq!(&IsingSpin::Down * &IsingSpin::Down, 1.0);
+    }
+
+    #[test]
+    fn heisemberg_spin_multiplies_correctly() {
+        assert_eq!(&HeisenbergSpin([1.0, 1.0, 1.0]) * &HeisenbergSpin([1.0, 1.0, 1.0]), 3.0);
     }
 }
