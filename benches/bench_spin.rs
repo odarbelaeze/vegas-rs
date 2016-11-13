3bab53a9e0414b5afe7ead0ed95a782435eed46e
Oscar David Arbeláez <odarbelaeze@gmail.com>
2016-11-13 10:29:19 -0500

    Implement random spin generation

---
+++ benches/bench_spin.rs
@@ -0,0 +1,24 @@
+#![feature(test)]
+extern crate vegas_rs;
+extern crate test;
+
+use vegas_rs::{Spin, IsingSpin, HeisenbergSpin};
+
+
+#[bench]
+fn create_10k_ising_spins(b: &mut test::Bencher) {
+    b.iter(|| {
+        for _ in 0..10_000 {
+            let _spin = IsingSpin::rand();
+        }
+    });
+}
+
+#[bench]
+fn create_10k_heisenberg_spins(b: &mut test::Bencher) {
+    b.iter(|| {
+        for _ in 0..10_000 {
+            let _spin = HeisenbergSpin::rand();
+        }
+    });
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,24 +2,34 @@
 //! atomistic Monte Carlo simulation program to deal with magnetic properties
 //! of materials.
 
+extern crate rand;
+
 use std::ops::Mul;
+use rand::distributions::{IndependentSample, Normal, Range};
 
 
 /// This trait specifies what a spin is for me.
 pub trait Spin
     where for<'a, 'b> &'a Self: Mul<&'b Self, Output = f64>
 {
+    /// New up an up Spin, this depends on what you're calling up.
     fn up() -> Self;
+
+    /// New up a down Spin, this depends on what you're calling down, it should
+    /// be anti parallel to `Spin::up()`.
     fn down() -> Self;
 
-    /// This method should return a spin based on an old spin reference
-    ///
-    /// Examples:
-    ///
-    /// ```rust,ignore
-    /// let old = Spin::up();
-    /// let new = Spin::perturbation_of(old);
-    /// ```
+    /// New up a random spin.
+    fn rand() -> Self;
+}
+
+
+/// This trait represents a spin which can be created as a perturbation of
+/// another, useful for things like a Metropolis algorithm.
+pub trait PerturbableSpin: Spin
+    where for<'a, 'b> &'a Self: Mul<&'b Self, Output = f64>
+{
+    /// New up a spin which is the perturbation of other.
     fn perturbation_of(other: &Self) -> Self;
 }
 
@@ -28,7 +38,6 @@ pub enum IsingSpin {
     Up,
     Down,
 }
-
 
 impl Spin for IsingSpin {
     fn up() -> IsingSpin {
@@ -39,6 +48,15 @@ impl Spin for IsingSpin {
         IsingSpin::Down
     }
 
+    fn rand() -> IsingSpin {
+        let range = Range::new(0f64, 1f64);
+        let mut rng = rand::thread_rng();
+        let r = range.ind_sample(&mut rng);
+        if r < 0.5f64 { IsingSpin::Up } else { IsingSpin::Down }
+    }
+}
+
+impl PerturbableSpin for IsingSpin {
     fn perturbation_of(other: &IsingSpin) -> IsingSpin {
         use IsingSpin::{Up, Down};
         match *other {
@@ -47,7 +65,6 @@ impl Spin for IsingSpin {
         }
     }
 }
-
 
 impl<'a, 'b> Mul<&'a IsingSpin> for &'b IsingSpin {
     type Output = f64;
@@ -64,7 +81,6 @@ impl<'a, 'b> Mul<&'a IsingSpin> for &'b IsingSpin {
 
 pub struct HeisenbergSpin([f64; 3]);
 
-
 impl Spin for HeisenbergSpin {
     fn up() -> HeisenbergSpin {
         HeisenbergSpin([0f64, 0f64, 1f64])
@@ -74,12 +90,26 @@ impl Spin for HeisenbergSpin {
         HeisenbergSpin([0f64, 0f64, -1f64])
     }
 
+    fn rand() -> HeisenbergSpin {
+        let normal = Normal::new(0.0, 1.0);
+        let mut rng = rand::thread_rng();
+        let x = normal.ind_sample(&mut rng);
+        let y = normal.ind_sample(&mut rng);
+        let z = normal.ind_sample(&mut rng);
+        let n = [x, y, z].iter()
+            .map(|i| i * i)
+            .fold(0f64, |s, i| s + i)
+            .sqrt();
+        HeisenbergSpin([x/n, y/n, z/n])
+    }
+}
+
+impl PerturbableSpin for HeisenbergSpin {
     fn perturbation_of(other: &HeisenbergSpin) -> HeisenbergSpin {
         let &HeisenbergSpin(elems) = other;
         HeisenbergSpin([-elems[0], -elems[1], -elems[2]])
     }
 }
-
 
 impl<'a, 'b> Mul<&'a HeisenbergSpin> for &'b HeisenbergSpin {
     type Output = f64;
@@ -100,6 +130,7 @@ pub struct State<T: Spin>(Vec<T>) where for<'b, 'a> &'a T: std::ops::Mul<&'b T, 
 
 #[cfg(test)]
 mod tests {
+    use super::Spin;
     use super::IsingSpin;
     use super::HeisenbergSpin;
 
@@ -115,5 +146,12 @@ mod tests {
     fn heisemberg_spin_multiplies_correctly() {
         assert_eq!(&HeisenbergSpin([1.0, 1.0, 1.0]) * &HeisenbergSpin([1.0, 1.0, 1.0]),
                    3.0);
+    }
+
+    #[test]
+    fn heisenberg_spins_are_random() {
+        let HeisenbergSpin(a) = HeisenbergSpin::rand();
+        let HeisenbergSpin(b) = HeisenbergSpin::rand();
+        assert!(a != b);
     }
 }
