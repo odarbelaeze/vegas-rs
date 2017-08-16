337d328a726717171a1feaccecd2ab62c1b5d3a7
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-08-16 09:20:27 -0500

    Get some benches going on

---
+++ benches/bench_integration.rs
@@ -0,0 +1,44 @@
+#![feature(test)]
+
+#[macro_use] extern crate vegas_rs;
+extern crate test;
+
+use vegas_rs::state::{Spin, IsingSpin, HeisenbergSpin, State};
+use vegas_rs::energy::{Gauge, UniaxialAnisotropy};
+use vegas_rs::integrator::{Integrator, MetropolisIntegrator};
+
+
+#[bench]
+fn integration_of_1k_heisenberg_spin_with_gauge(b: &mut test::Bencher) {
+    let gauge = Gauge::new(1.0);
+    let integrator = MetropolisIntegrator::new(3.0);
+    let mut state = State::<HeisenbergSpin>::rand_with_size(1_000);
+    b.iter(|| {
+        state = integrator.step(&gauge, &state)
+    })
+}
+
+
+#[bench]
+fn integration_of_1k_ising_spin_with_gauge(b: &mut test::Bencher) {
+    let gauge = Gauge::new(1.0);
+    let integrator = MetropolisIntegrator::new(3.0);
+    let mut state = State::<IsingSpin>::rand_with_size(1_000);
+    b.iter(|| {
+        state = integrator.step(&gauge, &state)
+    })
+}
+
+
+#[bench]
+fn integration_of_1k_heisenberg_spin_with_compound_energy(b: &mut test::Bencher) {
+    let hamiltonian = hamiltonian!(
+        Gauge::new(1.0),
+        UniaxialAnisotropy::new(HeisenbergSpin::up(), 10.0)
+    );
+    let integrator = MetropolisIntegrator::new(3.0);
+    let mut state = State::<HeisenbergSpin>::rand_with_size(1_000);
+    b.iter(|| {
+        state = integrator.step(&hamiltonian, &state)
+    })
+}
--- benches/bench_spin.rs
+++ benches/bench_spin.rs
@@ -6,26 +6,26 @@ use vegas_rs::state::{Spin, IsingSpin, HeisenbergSpin, State};
 
 
 #[bench]
-fn create_10k_ising_spins(b: &mut test::Bencher) {
+fn create_1k_ising_spins(b: &mut test::Bencher) {
     b.iter(|| {
-        for _ in 0..10_000 {
+        for _ in 0..1_000 {
             let _spin = IsingSpin::rand();
         }
     });
 }
 
 #[bench]
-fn create_10k_heisenberg_spins(b: &mut test::Bencher) {
+fn create_1k_heisenberg_spins(b: &mut test::Bencher) {
     b.iter(|| {
-        for _ in 0..10_000 {
+        for _ in 0..1_000 {
             let _spin = HeisenbergSpin::rand();
         }
     });
 }
 
 #[bench]
-fn create_a_10k_spin_state(b: &mut test::Bencher) {
+fn create_a_1k_spin_state(b: &mut test::Bencher) {
     b.iter(|| {
-        let _state = State::<HeisenbergSpin>::rand_with_size(10_000);
+        let _state = State::<HeisenbergSpin>::rand_with_size(1_000);
     })
 }
