8c416cb89daf4f1f2d9eaa3e3578097fc4ee7123
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-08-31 08:49:59 -0500

    Keep a reference to a xorshift generator

--- benches/bench_integration.rs
+++ benches/bench_integration.rs
@@ -11,7 +11,7 @@ use vegas_rs::integrator::{Integrator, MetropolisIntegrator};
 #[bench]
 fn integration_of_1k_heisenberg_spin_with_gauge(b: &mut test::Bencher) {
     let gauge = Gauge::new(1.0);
-    let integrator = MetropolisIntegrator::new(3.0);
+    let mut integrator = MetropolisIntegrator::new(3.0);
     let mut state = State::<HeisenbergSpin>::rand_with_size(1_000);
     b.iter(|| {
         state = integrator.step(&gauge, &state)
@@ -22,7 +22,7 @@ fn integration_of_1k_heisenberg_spin_with_gauge(b: &mut test::Bencher) {
 #[bench]
 fn integration_of_1k_ising_spin_with_gauge(b: &mut test::Bencher) {
     let gauge = Gauge::new(1.0);
-    let integrator = MetropolisIntegrator::new(3.0);
+    let mut integrator = MetropolisIntegrator::new(3.0);
     let mut state = State::<IsingSpin>::rand_with_size(1_000);
     b.iter(|| {
         state = integrator.step(&gauge, &state)
@@ -36,7 +36,7 @@ fn integration_of_1k_heisenberg_spin_with_compound_energy(b: &mut test::Bencher)
         Gauge::new(1.0),
         UniaxialAnisotropy::new(HeisenbergSpin::up(), 10.0)
     );
-    let integrator = MetropolisIntegrator::new(3.0);
+    let mut integrator = MetropolisIntegrator::new(3.0);
     let mut state = State::<HeisenbergSpin>::rand_with_size(1_000);
     b.iter(|| {
         state = integrator.step(&hamiltonian, &state)
--- src/integrator.rs
+++ src/integrator.rs
@@ -1,23 +1,29 @@
 extern crate rand;
+
 use rand::distributions::{IndependentSample, Range};
+use rand::{Rng, XorShiftRng};
 
 use state::{Spin, State};
 use energy::EnergyComponent;
 
 
 pub trait Integrator<S: Spin, T: EnergyComponent<S>> {
-    fn step(&self, energy: &T, state: &State<S>) -> State<S>;
+    fn step(&mut self, energy: &T, state: &State<S>) -> State<S>;
 }
 
 
 pub struct MetropolisIntegrator {
+    rng: XorShiftRng,
     temp: f64,
 }
 
 
 impl MetropolisIntegrator {
     pub fn new(temp: f64) -> MetropolisIntegrator {
-        MetropolisIntegrator { temp: temp }
+        MetropolisIntegrator {
+            temp: temp,
+            rng: XorShiftRng::new_unseeded(),
+        }
     }
 
     pub fn temp(&self) -> f64 {
@@ -38,12 +44,11 @@ impl<S, T> Integrator<S, T> for MetropolisIntegrator where
     S: Spin + Clone,
     T: EnergyComponent<S>
 {
-    fn step(&self, energy: &T, state: &State<S>) -> State<S> {
+    fn step(&mut self, energy: &T, state: &State<S>) -> State<S> {
         let mut new_state = (*state).clone();
         let sites = Range::new(0, new_state.len());
-        let mut rng = rand::thread_rng();
         for _ in 0..new_state.len() {
-            let site = sites.ind_sample(&mut rng);
+            let site = sites.ind_sample(&mut self.rng);
             let old_energy = energy.energy(&new_state, site);
             new_state.set_at(site, Spin::rand());
             let new_energy = energy.energy(&new_state, site);
@@ -51,7 +56,7 @@ impl<S, T> Integrator<S, T> for MetropolisIntegrator where
             if delta < 0.0 {
                 continue
             }
-            if rand::random::<f64>() < (- delta / self.temp).exp() {
+            if self.rng.gen::<f64>() < (- delta / self.temp).exp() {
                 continue
             }
             new_state.set_at(site, state.at(site).clone());
