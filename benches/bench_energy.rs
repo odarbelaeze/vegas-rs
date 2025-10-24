9cca244488434a370cebfd81674d488d385b2e18
Oscar Arbelaez <oscar@arbelaez.dev>
2025-10-24 21:38:32 +0100

    fix benches

--- benches/bench_energy.rs
+++ benches/bench_energy.rs
@@ -5,13 +5,15 @@ extern crate vegas;
 
 use vegas::hamiltonian::{Compound, Gauge, Hamiltonian, UniaxialAnisotropy};
 use vegas::state::{HeisenbergSpin, Spin, State};
+use vegas::thermostat::Thermostat;
 
 #[bench]
 fn gauge_energy_of_a_1_k_heisenberg_state(b: &mut test::Bencher) {
     let state = State::<HeisenbergSpin>::rand_with_size(&mut rand::rng(), 1_000);
     let gauge = Gauge::new(1.0);
+    let thermostat = Thermostat::near_zero();
     b.iter(|| {
-        let energy = gauge.total_energy(&state);
+        let energy = gauge.total_energy(&thermostat, &state);
         assert!(energy == 1_000.0);
     })
 }
@@ -20,8 +22,9 @@ fn gauge_energy_of_a_1_k_heisenberg_state(b: &mut test::Bencher) {
 fn anisotropy_energy_of_a_1k_heisenberg_state(b: &mut test::Bencher) {
     let state = State::<HeisenbergSpin>::up_with_size(1_000);
     let anisotropy = UniaxialAnisotropy::new(HeisenbergSpin::up(), 1.0);
+    let thermostat = Thermostat::near_zero();
     b.iter(|| {
-        let energy = anisotropy.total_energy(&state);
+        let energy = anisotropy.total_energy(&thermostat, &state);
         assert!(energy == 1_000.0);
     })
 }
@@ -32,8 +35,9 @@ fn compound_energy_hit_1k_heisenberg(b: &mut test::Bencher) {
     let gauge = Gauge::new(1.0);
     let anisotropy = UniaxialAnisotropy::new(HeisenbergSpin::up(), 1.0);
     let compound = Compound::new(gauge, anisotropy);
+    let thermostat = Thermostat::near_zero();
     b.iter(|| {
-        let energy = compound.total_energy(&state);
+        let energy = compound.total_energy(&thermostat, &state);
         assert!(energy == 2_000.0);
     })
 }
